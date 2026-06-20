f5a10a05b74585773c36665d4329565e729a6abc
Anthony Luisaga <luisaga.anthony@gmail.com>
2026-06-20 20:05:09 +0800

    Initial Spring Boot project

---
+++ src/test/java/com/tweebs/simplear/SimplearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweebs.simplear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
