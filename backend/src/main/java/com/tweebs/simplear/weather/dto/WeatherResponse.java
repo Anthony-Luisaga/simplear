d2291479b5debc5491b8cebac2c35db235e92c08
Anthony Luisaga <luisaga.anthony@gmail.com>
2026-06-21 13:06:20 +0800

    Integrate Open-Meteo API

--- backend/src/main/java/com/tweebs/simplear/weather/dto/WeatherResponse.java
+++ backend/src/main/java/com/tweebs/simplear/weather/dto/WeatherResponse.java
@@ -1,27 +1,52 @@
 package com.tweebs.simplear.weather.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@Setter
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherResponse {
 
-//	@JsonProperty("current_weather")
-	private CurrentWeather current_weather;
+	private double latitude;
+	private double longitude;
+
+	@JsonProperty("generationtime_ms")
+	private double generationTimeMs;
+
+
+	@JsonProperty("utc_offset_seconds")
+	private double utcOffsetSeconds;
+
+	private String timezone;
+
+	@JsonProperty("timezone_abbreviation")
+	private String timezoneAbbreviation;
+
+	private double elevation;
+
+	@JsonProperty("current_weather")
+	private CurrentWeather currentWeather;
 
 	@Setter
 	@Getter
 	public static class CurrentWeather {
 		private double temperature;
-		private double windspeed;
 
-	}
+		@JsonProperty("windspeed")
+		private double windSpeed;
 
-	public CurrentWeather getCurrent_weather() {
-		return current_weather;
-	}
+		@JsonProperty("winddirection")
+		private double windDirection;
 
-	public void setCurrent_weather(CurrentWeather current_weather) {
-		this.current_weather = current_weather;
+		@JsonProperty("weathercode")
+		private int weatherCode;
+
+		@JsonProperty("is_day")
+		private boolean isDay;
+
+		private String time;
 	}
 }
