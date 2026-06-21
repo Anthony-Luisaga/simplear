cf1b74c8d28d4f68574640fb3fbb55be30d4f0a0
Anthony Luisaga <luisaga.anthony@gmail.com>
2026-06-21 13:34:33 +0800

    Add Basic logging

--- backend/src/main/java/com/tweebs/simplear/config/WebClientConfig.java
+++ backend/src/main/java/com/tweebs/simplear/config/WebClientConfig.java
@@ -1,12 +1,19 @@
 package com.tweebs.simplear.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
+
+	private static final Logger log =
+		LoggerFactory.getLogger(WebClientConfig.class);
 
 
 	@Bean
@@ -14,7 +21,34 @@ public class WebClientConfig {
 	public WebClient weatherWebClient() {
 		return WebClient.builder()
 			.baseUrl("https://api.open-meteo.com")
+			.filter(logRequest())
+			.filter(logResponseBody())
 			.build();
+	}
+
+	private ExchangeFilterFunction logRequest() {
+		return ExchangeFilterFunction.ofRequestProcessor(request -> {
+			log.info(
+				"HTTP {} {}",
+				request.method(),
+				request.url()
+			);
+			return Mono.just(request);
+		});
+	}
+
+	private ExchangeFilterFunction logResponseBody() {
+		return ExchangeFilterFunction.ofResponseProcessor(response ->
+			response.bodyToMono(String.class)
+				.doOnNext(body ->
+					log.debug("HTTP response body: {}", body)
+				)
+				.map(body ->
+					response.mutate()
+						.body(body)
+						.build()
+				)
+		);
 	}
 
 }
